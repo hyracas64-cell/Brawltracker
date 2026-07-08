0b60c60af8ce4edd34dde840f988579e4501d95a
hyracas64-cell <hyracas64@gmail.com>
2026-07-09 01:24:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,29 @@
 import discord
 import os
-from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Connecté en tant que {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+print("BOT STARTING...")
 
 TOKEN = os.getenv("TOKEN")
 
 if TOKEN is None:
-    print("TOKEN MANQUANT")
-else:
-    bot.run(TOKEN)
+    print("❌ TOKEN MANQUANT")
+    exit(1)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"✅ Connecté en tant que {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "!ping":
+        await message.channel.send("pong")
+
+client.run(TOKEN)
